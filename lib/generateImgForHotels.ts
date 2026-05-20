add6b574978881cef9caa5819dfb0b4641e6d066
shahnawaz khan <khan.dev006@gmail.com>
2026-05-20 21:13:20 +0530

    feat: add generateImgForHotels function to fetch images for hotel listings

---
+++ lib/generateImgForHotels.ts
@@ -0,0 +1,15 @@
+import { Hotel } from "@/types/trip";
+import { fetchImage } from "@/utils/fetchImage";
+
+export const generateImgForHotels = async (hotels: Hotel[]) => {
+  return await Promise.all(
+    hotels.map(async (hotel) => {
+      const placeName = hotel.hotel_name + " " + hotel.hotel_address;
+      const imageUrl = await fetchImage(placeName, hotel.geo_coordinates);
+      return {
+        ...hotel,
+        hotel_image_url: imageUrl,
+      };
+    }),
+  );
+};
